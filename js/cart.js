ac172cc7f839903de54aac1149ad796efb8d959f
Pauline Bacle <bacle-pauline@orange.fr>
2021-04-05 02:48:57 +0200

    commenting js files 5/5

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,10 @@
 /********* TOOLS **********/
-function isValid(item,regex){
+function isValid(item,regex){ /*check if the value of an item (should be used for input for examples) matches with the regex*/
     if(!item.value.match(regex)){
-        item.parentElement.firstElementChild.classList.add("formatError");
+        item.parentElement.firstElementChild.classList.add("formatError"); /* allows for error msg to be added below item */
         return false ; 
     }else {
-        if(item.parentElement.firstElementChild.classList.contains("formatError")){
+        if(item.parentElement.firstElementChild.classList.contains("formatError")){ /* if a error msg existed for this item, it should be removed now it's ok */
             item.parentElement.firstElementChild.classList.remove("formatError"); 
         }
         return true;
@@ -12,7 +12,91 @@ function isValid(item,regex){
 }
 /**************************/
 
+/**** PAGE COMPOSITION ****/
+var nbItems = updateCartCounter(); 
+if( nbItems != 0 ){ /* CASE 1 : cart is not empty */
+    document.querySelectorAll("section").forEach(item => {item.style.display = "flex"; }) 
+    var total = 0 ;
+    var orderTable = []; /* this array will contain all id of items in cart  */
+    for(let i = 0 ; i < localStorage.length ; i++){
+        if(/cart-/.test(localStorage.key(i)) ){ /* only reading entries in localStorage that relates to the cart  */
+            var typeItem = localStorage.key(i).match(/cart-([a-z]+)/i)[1];
+            JSON.parse(localStorage[localStorage.key(i)]).elements.forEach( item => {
+                orderTable.push(item.id);
+                requestItem(typeItem, item.id)
+                .then( data => {            
+                        var response = JSON.parse(data.responseText);
+                    var tr = document.createElement('tr');
+
+                    document.querySelector(".item-price").innerText= updatePrice(response.price);
+                    document.querySelector(".item-price").id = "price-" + response.price;
+        
+                    tr.innerHTML = '<td>'+response.name+'</td><td class="nowrap item-price" id="price-'+ response.price +'">'+ updatePrice(response.price)+'</td><td>'
+                        +item.count+'</td><td class="nowrap item-price" id="price-'+item.count*response.price+'">'+updatePrice(item.count*response.price)+'</td>';
+                    document.querySelector("#cart-table tbody").append(tr);
+                    total += item.count*response.price ;
+                    document.querySelector("#table-total :last-child").id = "price-" + total ;
+                    document.querySelector("#table-total :last-child").innerHTML = updatePrice(total);
+                    /* Dont forget about id of all price related elements, needed for updating when selecting another currency */
+    
+                    if(i == localStorage.length - 1){ /* When infos of the last item of the cart has been added tothe table*/
+                        fadeOutEffect(".loader") /* the loader should disappear */
+                        .then(() => {
+                            document.querySelector("table#cart-table").hidden = false; /* Table should be made visible */
+                            document.querySelector('button[type="submit"]').classList.remove("unclickable"); /* submit button should be made clickable*/
+                        })
+                    };
+
+
+                } )               
+            })           
+        }
+    }    
+}else{ /* CASE 2 : cart empty, error message displayed */
+    document.querySelectorAll("section").forEach(item => {item.style.display = "none"; }) 
+    document.querySelectorAll(".emptyMsg").forEach(item => {item.hidden = false; }) 
+    document.querySelector("main").classList.add("center");
+}
+/**************************/
+
 /****** REQUESTS API ******/
+document.querySelector('button[type="submit"]').addEventListener("click", function(e){ /* Event to send order */
+    e.preventDefault();
+    if( document.querySelector("form").reportValidity() && !e.target.classList.contains("unclickable")){ /* first checking if all required inputs are filled and submit button is avalaible  */
+        /* Checking all inputs */
+        let isFirstNameValid = isValid(document.getElementById('firstnameUser'),/^\s*[a-z]+\s*$/i);
+        let isLastNameValid = isValid(document.getElementById('nameUser'),/^\s*[a-z]+\s*$/i);
+        let isAddressValid =  isValid(document.getElementById('addressUser'),/^\s*[a-z0-9\s]+\s*$/i);
+        let isCityValid = isValid(document.getElementById('cityUser'),/^\s*[a-z]+\s*$/i);
+        let isEmailValid = isValid(document.getElementById('emailUser'),/^\s*[a-z0-9._-]+@[a-z0-9._-]+.[a-z]{2,}\s*$/i);
+
+        if(isEmailValid && isFirstNameValid && isLastNameValid && isCityValid && isAddressValid){
+            var order = { /* creating JSON containing infos of the customer */
+                contact : {
+                    firstName: document.getElementById('firstnameUser').value,
+                    lastName: document.getElementById('nameUser').value,
+                    address:document.getElementById('addressUser').value,
+                    city: document.getElementById('cityUser').value,
+                    email: document.getElementById('emailUser').value        
+                },
+                products : orderTable /* contains id of all items that were ordered */
+            }
+            
+            var typeItem = Object.keys(localStorage)[Object.keys(localStorage).findIndex( x => /cart-/.test(x) )].match(/cart-([a-z]+)/)[1];
+            /* typeItem is needed to send request to the right API */
+            sendOrder(typeItem, order)
+            .then( data => {
+                localStorage.setItem("orderId",JSON.parse(data.responseText).orderId ); 
+                localStorage.setItem("orderTotal",document.querySelector("#table-total th:last-child").id.match(/[0-9]+/));
+                window.location.replace("confirmation.html"); 
+            } )
+            .catch( data => {
+                window.setTimeout(function(){window.location.replace("error.html")},250); 
+            })            
+        };
+    }   
+})
+
 function sendOrder(typeItem, order){
     return new Promise((resolve,reject) => {
         var urlAPI = "http://localhost:3000/api/" + typeItem + "/order"   ;
@@ -29,87 +113,5 @@ function sendOrder(typeItem, order){
         }
         request.onerror = () => reject();
     })
-}
-
-document.querySelector('button[type="submit"]').addEventListener("click", function(e){
-    e.preventDefault();
-    if( document.querySelector("form").reportValidity() && !e.target.classList.contains("unclickable")){
-        let isFirstNameValid = isValid(document.getElementById('firstnameUser'),/^\s*[a-z]+\s*$/i);
-        let isLastNameValid = isValid(document.getElementById('nameUser'),/^\s*[a-z]+\s*$/i);
-        let isAddressValid =  isValid(document.getElementById('addressUser'),/^\s*[a-z0-9\s]+\s*$/i);
-        let isCityValid = isValid(document.getElementById('cityUser'),/^\s*[a-z]+\s*$/i);
-        let isEmailValid = isValid(document.getElementById('emailUser'),/^\s*[a-z0-9._-]+@[a-z0-9._-]+.[a-z]{2,}\s*$/i);
-
-        if(isEmailValid && isFirstNameValid && isLastNameValid && isCityValid && isAddressValid){
-            var order = {
-                contact : {
-                    firstName: document.getElementById('firstnameUser').value,
-                    lastName: document.getElementById('nameUser').value,
-                    address:document.getElementById('addressUser').value,
-                    city: document.getElementById('cityUser').value,
-                    email: document.getElementById('emailUser').value        
-                },
-                products : orderTable
-            }
-            
-            var typeItem = Object.keys(localStorage)[Object.keys(localStorage).findIndex( x => /cart-/.test(x) )].match(/cart-([a-z]+)/)[1];
-
-            sendOrder(typeItem, order)
-            .then( data => {
-                localStorage.setItem("orderId",JSON.parse(data.responseText).orderId );
-                localStorage.setItem("orderTotal",document.querySelector("#table-total th:last-child").id.match(/[0-9]+/));
-                window.location.replace("confirmation.html"); 
-
-            } )
-            .catch( data => {
-                window.setTimeout(function(){window.location.replace("error.html")},250); 
-            })            
-        };
-    }   
-})
-/**************************/
-
-/**** PAGE COMPOSITION ****/
-var nbItems = updateCartCounter();
-if( nbItems != 0 ){
-    document.querySelectorAll("section").forEach(item => {item.style.display = "flex"; }) 
-    var total = 0 ;
-    var orderTable = [];
-    for(let i = 0 ; i < localStorage.length ; i++){
-        if(/cart-/.test(localStorage.key(i)) ){
-            var typeItem = localStorage.key(i).match(/cart-([a-z]+)/i)[1];
-            JSON.parse(localStorage[localStorage.key(i)]).elements.forEach( item => {
-                orderTable.push(item.id);
-                requestItem(typeItem, item.id)
-                .then( data => {            
-                    if(!document.querySelector(".loader").classList.contains("hidden")){
-                        fadeOutEffect(".loader")
-                        .then(() => {
-                            document.querySelector("table#cart-table").hidden = false;
-                            document.querySelector('button[type="submit"]').classList.remove("unclickable");
-                        })
-                    };
-    
-                    var response = JSON.parse(data.responseText);
-                    var tr = document.createElement('tr');
-
-                    document.querySelector(".item-price").innerText= updatePrice(response.price);
-                    document.querySelector(".item-price").id = "price-" + response.price;
-        
-                    tr.innerHTML = '<td>'+response.name+'</td><td class="nowrap item-price" id="price-'+ response.price +'">'+ updatePrice(response.price)+'</td><td>'
-                        +item.count+'</td><td class="nowrap item-price" id="price-'+item.count*response.price+'">'+updatePrice(item.count*response.price)+'</td>';
-                    document.querySelector("#cart-table tbody").append(tr);
-                    total += item.count*response.price ;
-                    document.querySelector("#table-total :last-child").id = "price-" + total ;
-                    document.querySelector("#table-total :last-child").innerHTML = updatePrice(total);
-                } )               
-            })           
-            count = +JSON.parse(localStorage[localStorage.key(i)]).total;
-        }
-    }    
-}else{
-    document.querySelectorAll("section").forEach(item => {item.style.display = "none"; }) 
-    document.querySelectorAll(".emptyMsg").forEach(item => {item.hidden = false; }) 
-    document.querySelector("main").classList.add("center");
 }
 /**************************/
--- js/products.js
+++ js/products.js
@@ -1,34 +1,37 @@
 /******** TOOLS ********/
-function checkCart() {
+function checkCart(typeItem) { /* checks if a cart for this type of items already exists in localStorage or not   */
     return new Promise( (resolve,reject) => {
         if(localStorage.getItem("cart-"+ typeItem)){
-            resolve(true);
+            resolve(true); /*  a cart for this type of items already exists  */
         }else if(Object.keys(localStorage).map( x => /cart-/.test(x)).includes(true) ){
             var oldType = Object.keys(localStorage)[Object.keys(localStorage).findIndex( x => /cart-/.test(x))].match(/cart-([a-z]+)/)[1] ;
-            reject(oldType);
+            reject(oldType); /*  a cart for another type of items already exists */
         }else{
-            resolve(false);
+            resolve(false); /* no cart exists */
         }
     })
 }
 /**************************/
 
 /**** PAGE COMPOSITION ****/
-const url = window.location.search; 
-const urlParams = new URLSearchParams(url);
+const urlParams = new URLSearchParams(window.location.search);
 const typeItem = urlParams.get('type');
-if(urlParams.has('id') && urlParams.has('type') && urlParams.getAll('id')[0].length != 0 && urlParams.getAll('type')[0].length != 0){
-    requestItem(typeItem, urlParams.get('id'))
-    .then( data => {
+if(urlParams.has('id') && urlParams.has('type')  /* at least 1 type and 1 id */
+&& urlParams.getAll('id').length == 1 && urlParams.getAll('type').length == 1 /* only 1 type and 1 id */
+&& urlParams.getAll('id')[0].length != 0 && urlParams.getAll('type')[0].length != 0){ /* id and type are not null */
+    requestItem(typeItem, urlParams.get('id')) 
+    .then( data => { /* Item found by API request */
         var response = JSON.parse(data.responseText);
                   
         document.getElementById("item-image").src = response.imageUrl;
         loadImage(document.getElementById("item-image"))
-        .then( () => {
+        .then( () => { /* once image is loaded, page is displayed */
             fadeOutEffect(".loader");
-            document.querySelectorAll(".item-name").forEach(item => {item.innerText = response.name ;}) ;
+            /*Secondary nav bar updated as follow :  */
             document.querySelector("#categorie a").innerText = Object.values(categories)[Object.keys(categories).findIndex( x => x === typeItem)];
             document.querySelector("#categorie a").href = "index.html#"+typeItem;
+            /* Item infos updated as follow : */
+            document.querySelectorAll(".item-name").forEach(item => {item.innerText = response.name ;}) ;
             document.getElementById("item-description").innerText= response.description;
             document.querySelector(".item-price").innerText= updatePrice(response.price);
             document.querySelector(".item-price").id = "price-" + response.price;
@@ -45,10 +48,10 @@ if(urlParams.has('id') && urlParams.has('type') && urlParams.getAll('id')[0].len
             document.querySelector("#item").style.display = "flex";            
         })
     })
-    .catch( () => { 
+    .catch( () => { /* Nothing found by API request */
         window.setTimeout(function(){window.location.replace("error.html")},250); 
     })        
-}else{
+}else{ /* Issue in the url : the id and/or the type of the item is missing or multiples */
     document.querySelector("#nav-product").hidden = true ;
     document.querySelector(".loader").style.display =  'none' ;
     document.querySelector(".errorDialog").style.display = 'flex';
@@ -57,28 +60,27 @@ if(urlParams.has('id') && urlParams.has('type') && urlParams.getAll('id')[0].len
 /**************************/
 
 /******** BUTTONS  ********/
-document.querySelector(".btn-deleteCart").addEventListener("click", function(e) {
+document.querySelector(".btn-deleteCart").addEventListener("click", function(e) { /* Replace current cart in localStorage by new one */
     e.preventDefault();
-    localStorage.removeItem("cart-"+Object.keys(localStorage)[Object.keys(localStorage).findIndex( x => /cart-/.test(x))].match(/cart-([a-z]+)/)[1]
-    );
-    document.querySelector(".Dialog.existingCart").style.display = "none";
-    document.querySelector(".btn-addToCart").click();    
+    localStorage.removeItem("cart-"+Object.keys(localStorage)[Object.keys(localStorage).findIndex( x => /cart-/.test(x))].match(/cart-([a-z]+)/)[1] );
+    document.querySelector(".Dialog.existingCart").style.display = "none"; /* Removing dialog panel for warning of previous existing cart */
+    document.querySelector(".btn-addToCart").click();    /* New cart is created */
 })
 
-document.querySelector(".btn-addToCart").addEventListener("click", function(e) {
+document.querySelector(".btn-addToCart").addEventListener("click", function(e) { /* Add the item in the cart */
     e.preventDefault();
     var form = document.querySelector("form") ;
     form.reportValidity();   
-    if(form.checkValidity() ){
+    if(form.checkValidity() ){ /* makes sure that an option is selected */
 
-        checkCart()
+        checkCart(typeItem) 
         .then( (data) => {
  
             document.querySelector("#item h1").hidden = true;
             document.querySelector(".recap.Dialog").style.display = 'flex';
             document.querySelector(".recap.Dialog span.item-option").innerText = document.getElementById("item-option").value;
             
-            if(data){
+            if(data){ /* updating the cart already existing */
                 var cartObj = JSON.parse(localStorage.getItem("cart-"+ typeItem));
                 var index = cartObj.elements.findIndex( x => x.id === document.getElementById("item-id").value) ;
     
@@ -89,28 +91,27 @@ document.querySelector(".btn-addToCart").addEventListener("click", function(e) {
                 } 
                 cartObj.total = ++cartObj.total ;
                 localStorage.setItem("cart-"+ typeItem, JSON.stringify(cartObj));  
-            }else{
+            }else{ /* creating a cart */
                 localStorage.setItem("cart-"+ typeItem, JSON.stringify({total : '1',  
                 elements: [{ id : document.getElementById("item-id").value, count: '1',}]} ) );
             }
-    
-            document.getElementById("cart-counter").classList.add("addOne");    
-            updateCartCounter();
+            document.getElementById("cart-counter").classList.add("addOne"); /* creates an animation on the cart counter */
+            updateCartCounter(); /* update the cart counter */
         })
-        .catch( (oldType) => {
-            document.querySelector("#item h1").hidden = true;
+        .catch( (oldType) => { /* another cart already exists */
+            document.querySelector("#item h1").hidden = true; 
             document.querySelector(".item-type--old").innerText = Object.values(categories)[Object.keys(categories).findIndex( x => x === oldType)].toLowerCase() ;
             document.querySelector(".existingCart.Dialog").style.display = 'flex';
         })
     }
  })
 
- document.querySelectorAll(".btn-close").forEach(item => {
+ document.querySelectorAll(".btn-close").forEach(item => { /* close any dialog panel on which this button is */
     item.addEventListener("click", function(e){
         e.preventDefault();
         e.target.parentNode.parentNode.style.display = 'none';
         document.querySelector("#item h1").hidden = false;
-        if(document.getElementById("cart-counter").classList.contains("addOne") ){
+        if(document.getElementById("cart-counter").classList.contains("addOne") ){ /* if an item just got added in the cart, this class should be removed to allow for the animation if another one is added */
             document.getElementById("cart-counter").classList.remove("addOne")
         }
     })
